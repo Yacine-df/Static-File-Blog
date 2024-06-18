7c71b600e3cb68ccb828181d9c4e82374f77d11c
Yacine-df <ycn.diaf@gmail.com>
2024-06-18 14:40:34 +0200

    Add Custome Route Model Binding to grab files

---
+++ app/Http/Controllers/PostShowController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+
+
+class PostShowController extends Controller
+{
+
+    public function __invoke(Post $post)
+    {
+
+        dd($post);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PostShowController;
 use App\Http\Controllers\PostsIndexController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', PostsIndexController::class);
+Route::get('/{post}', PostShowController::class);
