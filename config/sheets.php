18e8d73e663a24457af3ab8ce98398ab436f278d
Yacine-df <ycn.diaf@gmail.com>
2024-06-18 12:43:28 +0200

    Set up Sheets package

--- config/sheets.php
+++ config/sheets.php
@@ -5,14 +5,12 @@ return [
 
     'collections' => [
 
-        /* An example collection. All keys are optional.
-
         'posts' => [
             'disk' => 'posts',
             'sheet_class' => App\Models\Post::class,
             'path_parser' => Spatie\Sheets\PathParsers\SlugWithDateParser::class,
             'content_parser' => Spatie\Sheets\ContentParsers\MarkdownParser::class,
-            'extension' => 'txt',
-        ], */
+            'extension' => 'md',
+        ],
     ],
 ];
